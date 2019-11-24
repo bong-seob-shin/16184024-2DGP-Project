d3894ed7c917e2e05af8d9704ba836b285d9297b
bong-seob-shin <56147393+bong-seob-shin@users.noreply.github.com>
2019-11-24 23:26:57 +0900

    발표준비

--- main_state.py
+++ main_state.py
@@ -37,7 +37,7 @@ def enter():
     BackGround_Width = 1280
     BackGround_Height = 960
     isaac = Isaac()
-    monster_count = 4
+    monster_count = 2
     gushers = [Gusher() for i in range (monster_count)]
     background = BackGround()
     door = Door()
